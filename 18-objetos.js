1637bac3f9ef80fbb1b5fd36966e485098d836d2
K1RIAN <mmian.alejandro@gmail.com>
2026-05-12 21:06:23 -0300

    inicio de objetos
    es como las struct de C

---
+++ 18-objetos.js
@@ -0,0 +1,12 @@
+// objetos coleccion de propiedades que se estructura de una forma muy parecida a un map
+
+//sintaxis es como un struct en C
+
+let person={
+    name: "ian",
+    age: 20,
+    alias: "kirian"
+}
+
+//acceso a propiedades
+
