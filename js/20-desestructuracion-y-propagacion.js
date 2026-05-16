808792df5244964b2ced48cf305121cd36d67fb8
K1RIAN <mmian.alejandro@gmail.com>
2026-05-16 14:33:22 -0300

    leccion 20

---
+++ js/20-desestructuracion-y-propagacion.js
@@ -0,0 +1,11 @@
+// Desestructuracion
+
+//sintaxis que nos permite extraer valores de arrays o objetos y guardarlos en variables
+
+myArray = [1,2,3,4]
+
+let person={
+    name:"ian",
+    age: 20,
+    alias: "kirian"
+}
